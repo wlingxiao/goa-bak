f660e7cd09c8bc07c73d0eee56629bf90ca0beef
wlingxiao <wlingxiao@outlook.com>
2018-04-26 12:38:10 +0800

    logging

---
+++ core/src/main/scala/goa/Logging.scala
@@ -0,0 +1,38 @@
+package goa
+
+import org.slf4j.{LoggerFactory, Logger => Slf4JLogger}
+
+private[goa] trait Logging {
+
+  protected lazy val logger = Logger(this)
+
+}
+
+private[goa] abstract class Logger {
+
+  def debug(msg: => String): Unit
+
+  def error(msg: => String, t: => Throwable): Unit
+
+}
+
+private[goa] object Logger {
+
+  private class Slf4JUnderlying(underlying: Slf4JLogger) extends Logger {
+    override def debug(msg: => String): Unit = {
+      if (underlying.isDebugEnabled) {
+        underlying.debug(msg)
+      }
+    }
+
+    override def error(msg: => String, t: => Throwable): Unit = {
+      if (underlying.isErrorEnabled) {
+        underlying.error(msg, t)
+      }
+    }
+  }
+
+  def apply(logging: Logging): Logger = {
+    new Slf4JUnderlying(LoggerFactory.getLogger(logging.getClass))
+  }
+}
